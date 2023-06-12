33a312d97dfc5e62cb86b15b30c44af947bb2a82
yamini saraswathi <103330658+yaminiakkala@users.noreply.github.com>
2023-06-12 20:30:23 +0530

    Add files via upload

---
+++ javabasics.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class javabasics{
+    public static void main(String args[]){
+     Scanner sc= new Scanner(System.in);
+     
+     int n=sc.nextInt();
+    //  if(n==2){
+    //     System.out.println("prime number");
+    //  }else{
+    //     int cnt=2;
+    //     for(int i=2;i<=Math.sqrt(n);i++){
+    //         if(n%i==0){
+    //             System.out.println("not a prime number");
+    //             cnt++;
+    //             break;
+    //         }
+    //     }
+    //     if(cnt==2){
+    //         System.out.println("prime number");
+    //     }
+    //  }
+    
+
+
+    }
+}
